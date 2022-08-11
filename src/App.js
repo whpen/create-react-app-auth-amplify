eab2ca40fb831b55ae0a8d55b559c1445681a305
whpen <whpen@hotmail.com>
2022-08-12 00:59:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import {Amplify} from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { AmplifyS3Image } from "@aws-amplify/ui-react";
+import { Storage } from 'aws-amplify';
+import { useEffect, useRef, useState } from 'react';
+
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
+
+function App() {
+  const ref = useRef(null);
+  const [files, setFiles] = useState([]);
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState();
+
+
+
+  const loadImages = () => {
+    Storage.list("")
+      .then((files) => {
+        console.log(files);
+        setFiles(files);
+      })
+      .catch((err) => {
+        console.log(err);
+      });    
+  }
+
+  useEffect(() => {
+    loadImages();
+   }, []);
+
+  const handleFileLoad = () => {
+    const filename = ref.current.files[0].name;
+    Storage.put(filename, ref.current.files[0], {
+      progressCallback: (progress) => {
+        setProgress(Math.round((progress.loaded / progress.total) * 100) + "%");
+        setTimeout(() => { setProgress() }, 1000);
+      }
+    })
+      .then(resp => {
+      console.log(resp);
+      loadImages();
+    }).catch(err => {console.log(err);});
+  }
+
+  const handleShow = (file) => {
+    Storage.get(file).then(resp => {
+      console.log(resp);
+      setImage(resp)
+    }).catch(err => { console.log(err); });
+  }
+
+  const handleDelete = (file) => {
+    Storage.remove(file).then(resp => {
+      console.log(resp);
+      loadImages();
+     }).catch(err => { console.log(err); });
+  }
+ return (
       <div className="App">
         <AmplifySignOut />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -25,12 +77,35 @@ class App extends Component {
           >
             Learn React
           </a>
-          <AmplifyS3Image imgKey="example.jpeg" />
-          
+          {/* <AmplifyS3Image imgKey="example.jpeg" /> */}
+          <h1>React AWS Storage Demo</h1>
+        <input ref={ref} type="file" onChange={handleFileLoad} />
+        { progress}
+        <table>
+          <thead>
+            <tr>
+              <td></td>
+              <td>Name</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            {files.map((file,i) => (
+              <tr key={file.key}>
+                <td>{i}</td>
+                <td>{file.key}</td>
+                <td>
+                  <button onClick={() => handleShow(file.key)}>Show</button>
+                  <button onClick={() => handleDelete(file.key)}>Delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <img src={image} width="600"/>
         </header>
       </div>
     );
   }
-}
+
 
 export default withAuthenticator(App);
